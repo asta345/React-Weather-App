59d18bf480c4bd5020d0e2c2d23a07595db53f5e
asta345 <81383112+asta345@users.noreply.github.com>
2021-11-29 17:14:10 +0530

    Add files via upload

---
+++ compnents/Property.jsx
@@ -0,0 +1,7 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Flex, Box, Text, Button } from '@chakra-ui/react';
+import {FaBed,FaBath} from 'react-icons/fa';
+import {BsGridFill}from 'react-icons/bs';
+import {GoVerified} from 'react-icons/goverified';
+import  millify from 'millify';
